708374803b5869049ec060f02cd0bc55d0e2f63c
lmunarozanetti-debug <lmunarozanetti@gmail.com>
2026-01-17 16:20:44 -0300

    Implement Heroi class with attack logic
    Added Heroi class with attack types based on character type.

--- heroi.js
+++ heroi.js
@@ -1,1 +1,24 @@
-
+class Heroi {
+     constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+      
+      switch (tipo) {
+         case "mago":
+            this.ataque = "magia"
+            break
+         case "ninja":
+            this.ataque = "shuriken"
+            break
+         case "clerigo":
+            this.ataque = "espantar mortos vivos"
+            break
+         case "guerreiro":
+            this.ataque = "espada"
+            break
+      }
+      }
+      }
+      let personagem = new Heroi("Cleiton" , "13" , "mago")
+      console.log("O personagem " + personagem.nome + " atacou com " + personagem.ataque + "." )
